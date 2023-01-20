b63f51684aa23abe87086e178c376d5a86a8a058
kira <korolkovakv@gmail.com>
2023-01-20 18:51:52 +0700

    Добавьте файлы проекта.

---
+++ Lab11/Controllers/CalcServiceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Lab11.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Lab11.Controllers {
+    public class CalcServiceController : Controller {
+        private readonly Random _random = new Random();
+        private readonly int _x;
+        private readonly int _y;
+        private readonly string _sum;
+        private readonly string _dif;
+        private readonly string _mult;
+        private readonly string _div;
+
+        public CalcServiceController() {
+            _x = _random.Next() % 11;
+            _y = _random.Next() % 11;
+            _sum = $"{_x} + {_y} = {_x + _y}";
+            _dif = $"{_x} - {_y} = {_x - _y}";
+            _mult = $"{_x} * {_y} = {_x * _y}";
+            _div = _y != 0 ? $"{_x} /  {_y} = {_x / _y}" : "Division by zero";
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult PassUsingModel() {
+            var model = new CalcModel(_x, _y, _sum, _dif, _mult, _div);
+            return View(model);
+        }
+
+        public IActionResult PassUsingViewData() {
+            ViewData["X"] = _x;
+            ViewData["Y"] = _y;
+            ViewData["Sum"] = _sum;
+            ViewData["Dif"] = _dif;
+            ViewData["Mult"] = _mult;
+            ViewData["Div"] = _div;
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag() {
+            ViewBag.X = _x;
+            ViewBag.Y = _y;
+            ViewBag.Sum = _sum;
+            ViewBag.Dif = _dif;
+            ViewBag.Mult = _mult;
+            ViewBag.Div = _div;
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly() {
+            var model = new CalcModel(_x, _y, _sum, _dif, _mult, _div);
+            return View(model);
+        }
+    }
+}
---
+++ Lab11/Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lab11 {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("default", "{controller=CalcService}/{action=Index}");
+            });
+        }
+    }
+}
